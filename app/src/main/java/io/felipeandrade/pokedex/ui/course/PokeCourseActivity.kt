dbd6cd77b666c0a5b565a25597d5b158953d9ecd
mateus <mateustostesdossantos@gmail.com>
2019-11-05 16:14:22 -0200

    Course Quebrando

--- app/src/main/java/io/felipeandrade/pokedex/ui/course/PokeCourseActivity.kt
+++ app/src/main/java/io/felipeandrade/pokedex/ui/course/PokeCourseActivity.kt
@@ -1,5 +1,8 @@
 package io.felipeandrade.pokedex.ui.course
-
+import android.view.View
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import io.felipeandrade.pokedex.R
 import io.felipeandrade.pokedex.ui.base.PokeBaseActivity
 import io.felipeandrade.pokedex.ui.menu.PokeMenuPresenter
@@ -9,7 +12,25 @@ import org.koin.core.parameter.parametersOf
 class PokeCourseActivity : PokeBaseActivity(), PokeCourseView {
 
     private val pokePresenter: PokeCoursePresenter by inject { parametersOf(this) }
-
+    private lateinit var webView: WebView
     override fun getPresenter() = pokePresenter
     override fun getLayoutRes() = R.layout.course_activity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_activity)
+
+        webView = findViewById(R.id.webPage)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://www.fiap.com.br/graduacao/tecnologo/jogos-digitais/")
+    }
+
+
+
+
 }
